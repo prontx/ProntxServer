dfa447c3226ee2371f000e2ca85a69e7a7cc4338
prontx <79801523+prontx@users.noreply.github.com>
2021-07-28 15:55:05 +0200

    The better version, now minor details, testing and the manual

--- bot.py
+++ bot.py
@@ -1,32 +1,38 @@
-# A bot for the Discord server prontx
+# Discord Server Bot Project
 # Author : Matsvei Hauryliuk, VUT FIT Student
 # Github : @prontx
+# July 2021
 
 # bot.py
-# Main driver file
+# The driver file for the bot
+
+
+
+# Section 1: needed imports and initial setup
+###################################################################################################
+
+
 
 # Imports the package containing math functions definitions
-from copy import deepcopy
 from util import calculator_functions 
-# Can be used like: calculator_functions.add(a, b)
 
-# To make requests to third party APIs
-from requests.models import Response, to_key_val_list
+# Allows to make requests to third party APIs
 import requests, json
 
-# To use the Google Translate APIs
+# Enables using the Google Translate APIs
 from googletrans import Translator
 
-# To work with environment files
+# Needed to work with environment files
 import os
 
 import discord
 from discord.ext import commands
 
 # Will be used to calculate current time
-from datetime import date, datetime
+from datetime import datetime
 
 from dotenv import load_dotenv
+
 # Reads key-value pairs from the .env file
 load_dotenv()
 
@@ -34,46 +40,60 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-# A required step to see all users
+# A required step so that the bot is able to see all users
 intents = discord.Intents.all()
 
 # Creating a bot while also specialising the command prefix symbols and the intents
 # so we're able to see all the users and not just bots
-bot = commands.Bot(command_prefix=('$', '?'), intents=intents)
+bot = commands.Bot(command_prefix=('$', '?', '!'), intents=intents)
 
+# Required to use the SQLite databases
+import sqlite3
+
+# To handle asynchronous accesses to shared variables
+import asyncio
+
+
+
+# Section 2: database setup
 ###################################################################################################
 
-# Imports required to use the SQLite databases
-import sqlite3
-from sqlite3 import Error
 
+
+# Creates a DB file in the same folder to store data
 database = r'bot.db'
 
+# SQL command to create a table <users> containing 4 fields:
+# Discord ID, Discord user name, channels user follows and date of first connection
 sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
                                 discord_ID integer PRIMARY KEY,
                                 user_name text NOT NULL,
                                 channels text,
-                                begin_date text NOT NULL,
-                                permissions text NOT NULL  
+                                begin_date text NOT NULL
                             );"""
 
-# Creating a database connection to a SQLite database
-conn = None
-    
+# Creating a connection to a SQLite database
 conn = sqlite3.connect(database)
 
-# Creating a cursor to operate on the table
+# Creating a cursor to perform actions with the table
 c = conn.cursor()
 
 # The code to create the table
 c.execute(sql_create_users_table)
 
+# For the changes to take place
 conn.commit()
 
+
+
+# Section 3: bot events and commands 
 ###################################################################################################
 
-# Command to calculate the given operation on the given numbers
+
+
+# Command to perform a certain(given) mathematical operation on the presented numbers
 # Uses the functions imported from the util package
+# Arguments: first number, second number, operation
 @bot.command()
 async def calculate(ctx, num1, num2, op):
     try:
@@ -83,37 +103,30 @@ async def calculate(ctx, num1, num2, op):
 
         if op == '+':
             result = calculator_functions.add(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '-':
             result = calculator_functions.subtract(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '*':
             result = calculator_functions.multiply(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '/':
             result = calculator_functions.divide(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '//':
             result = calculator_functions.divide_no_remainder(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '**':
             result = calculator_functions.power(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
         if op == '%':
             result = calculator_functions.modulo(num1, num2)
-
             await ctx.send(f'```The result of the operation is: {result}```')
 
     except ValueError:
@@ -124,80 +137,61 @@ async def calculate(ctx, num1, num2, op):
 
 # Command to translate the given word from one language to the other
 # Uses Google Translator APIs
+# Arguments: first language, second language, word or expression to be translated
 @bot.command()
 async def translate(ctx, firstlang, secondlang, *args):
     word = ''
-
     for i in range(len(args)):
         word += args[i]
-
         if i != (len(args) - 1):
             word += ' '
 
     import http.client
-
     conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
-
     payload = "q=" + word + "&format=text&target=" + secondlang +"&source=" + firstlang
-
     headers = {
         'content-type': "application/x-www-form-urlencoded",
         'accept-encoding': "application/gzip",
         'x-rapidapi-key': "03e84d1cebmsh8393172a48e2fb0p137f01jsn5239041898af",
         'x-rapidapi-host': "google-translate1.p.rapidapi.com"
     }
-
     conn.request("POST", "/language/translate/v2", payload, headers)
-
     res = conn.getresponse()
-
     data = res.read()
-
     result = data.decode('utf-8')
 
-    # Turns string into dictionary
+    # Turns the decoded string into dictionary for to be easier to process later
     result = eval(result)
 
+    # Gets the translation itself
     result = result["data"]["translations"][0]["translatedText"]
 
     await ctx.send(f'```The translation of your word/phrase is: {result}```')
 
-# Prints the manual
-@bot.command()
-async def manual(ctx):
-    await ctx.send(f'''```TBD```''')
-
 # Command to determine the temperature in a city given as argument
 # Uses third party geopy APIs
+# Argument: the city name
 @bot.command()
 async def weather(ctx, *args):
     try:
         city = ''
-
         for i in range(len(args)):
             city += args[i]
-
             if i != (len(args) - 1):
                 city += ' '
 
         from geopy.geocoders import Nominatim
-
         geolocator = Nominatim(user_agent='aurinko')
-
         location = geolocator.geocode(city)
 
         latitude = location.raw['lat']
-
         longitude = location.raw['lon']
 
         apiKey = '0676b93a6bd7fdb28d17cb06e21aa60b'
-
         url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (latitude, longitude, apiKey)
 
         response = requests.get(url)
-
         data = json.loads(response.text)
-
         currentTemp = data["current"]["temp"]
 
         await ctx.send(f'```Current temperature in {city} is: {currentTemp}°C```')
@@ -207,40 +201,39 @@ async def weather(ctx, *args):
 
 
 # Handles every time the client connects to the server
-# Adds all the users' data to the database
+# Adds all the users' data to the dtabase
 @bot.event
 async def on_connect():
     for guild in bot.guilds:
         for member in guild.members:
             # Field 4 of the database
+            # If the user is already there, date won't be changed
+            # Otherwise is added
             firstSessionDate = datetime.now()
 
             # Field 1 of the database
+            # Gets the Discord ID
             global userID
             userID = member.id
 
             # Field 2 of the database
+            # Gets the Discord user name
             userName = member.name
 
-            # Field 5 of the database
-            permissions = 'User'
-
             # Field 3 of the database
+            # The channels user follows
             channels = 'General'
 
             conn = sqlite3.connect(database)
-
-            # Creating a cursor to operate on the table
             c = conn.cursor()  
 
-            # The code to create the table
-            c.execute("""INSERT or IGNORE INTO users VALUES (?, ?, ?, ?, ?)""",
-                (userID, userName, channels, firstSessionDate, permissions))
+            # The code to apply changes to the table
+            c.execute("""INSERT or IGNORE INTO users VALUES (?, ?, ?, ?)""",
+                (userID, userName, channels, firstSessionDate))
 
-            # For the changes to take place
             conn.commit()
 
-# To be used later
+# To be used later ??????????????????????? WTF
 channels = 'General '
 
 # Called whenever a member joins the server
@@ -256,41 +249,34 @@ async def on_member_join(member):
             # Field 2 of the database
             userName = member.name
 
-            # Field 5 of the database
-            permissions = 'User'
-
             # Field 3 of the database
             channels = 'General'
 
             conn = sqlite3.connect(database)
-
-            # Creating a cursor to operate on the table
             c = conn.cursor()  
 
             # The code to create the table
             c.execute("""INSERT or IGNORE INTO users VALUES (?, ?, ?, ?, ?)""",
-                (userID, userName, channels, firstSessionDate, permissions))
+                (userID, userName, channels, firstSessionDate))
 
-            # For the changes to take place
             conn.commit()
 
+# Called each time a message is sent
 @bot.event
 async def on_message(message):
     # Prints the info about the message author
     if message.content == '$me':
         conn = sqlite3.connect(database)
-
-        # Creating a cursor to operate on the table
         c = conn.cursor()  
 
+        # Searches through the database using the Discord ID as identifier
         c.execute("SELECT * FROM users WHERE discord_ID=?", (message.author.id, ))
 
-        # Gets the row with the user's data
+        # Gets the row with the needed user's data
         row = c.fetchall()
 
         await message.channel.send(f'```{row}```')
 
-        # For the changes to take place
         conn.commit()
 
     # Should be here so the message is later sent to be processed by bot.command()
@@ -298,9 +284,6 @@ async def on_message(message):
 
 # To be later used in the voting
 answer = 0
-
-import asyncio
-lock = asyncio.Lock()
 
 # The code for implementation of reaction roles
 @bot.event
@@ -310,11 +293,7 @@ async def on_raw_reaction_add(payload):
     if payload.channel_id == channel and payload.message_id == message:
         guild = bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
-
-        # This is the problem
         emoji = payload.emoji
-
-        import copy
         global channels
 
         if str(emoji.name) == '🎥' :
@@ -358,36 +337,23 @@ async def on_raw_reaction_add(payload):
                 print('Something wrong with the role')
 
         conn = sqlite3.connect(database)
-
-        # Creating a cursor to operate on the table
         c = conn.cursor()  
 
-        # The code to create the table
+        # The code to apply changes to the table
         c.execute("""UPDATE users SET channels=? WHERE discord_ID=?""", (channels, member.id))
 
-        # For the changes to take place
         conn.commit()
 
     else:
-        global lock
+        global answer
 
-        await lock.acquire()
-        try:
-            global answer
+        if str(payload.emoji.name) == '1️⃣':
+            answer = answer + 1
 
-            if str(payload.emoji.name) == '1️⃣':
-                    answer = answer + 1
+        if str(payload.emoji.name) == '2️⃣':
+            answer = answer - 1           
 
-            print(answer)
-
-            if str(payload.emoji.name) == '2️⃣':
-                answer = answer - 1           
-        finally:
-            lock.release()
-
-# Case reaction is removed
-
-
+# In case reaction is removed
 @bot.event
 async def on_raw_reaction_remove(payload):
     channel = 867854256647176252
@@ -395,11 +361,7 @@ async def on_raw_reaction_remove(payload):
     if payload.channel_id == channel and payload.message_id == message:
         guild = bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
-
-        # This is the problem
         emoji = payload.emoji
-
-        import copy
         global channels
 
         channels = str(channels)
@@ -441,36 +403,40 @@ async def on_raw_reaction_remove(payload):
             await member.remove_roles(role)
 
         conn = sqlite3.connect(database)
-
-        # Creating a cursor to operate on the table
         c = conn.cursor()  
 
         channels = str(channels)
 
-        # The code to create the table
+        # The code to apply changes to the table
         c.execute("""UPDATE users SET channels=? WHERE discord_ID=?""", (channels, member.id))
-
-        # For the changes to take place
         conn.commit()
 
-# Command to initiate a vote between two options given as arguments
+# Command to initiate a vote with two options given as arguments
 # Voting is over after 10 seconds and result is printed
+# Arguments: the voting question, the first option, the second option
 @bot.command()
 async def vote(ctx, question, choice1, choice2):
-    await ctx.send(f'```{question} \n Select between {choice1} or {choice2}```')
+    await ctx.send(f'```{question} \nSelect between {choice1} or {choice2}```')
 
-    import time
+    await asyncio.sleep(10)
 
-    time.sleep(5)
+    if answer > 0:
+        await ctx.send(f'```{choice1} wins!```')
+    elif answer < 0:
+        await ctx.send(f'```{choice2} wins!```')
+    else:
+        await ctx.send(f'```Tie!```')
 
-    global lock
-    await lock.acquire()
-    try:
+# Prints the manual
+@bot.command()
+async def manual(ctx):
+    await ctx.send(f'''```TBD```''')
 
-        global answer
 
-        await ctx.send(f'```{answer}```')
-    finally:
-        lock.release()
+
+# Section 4: running the bot 
+###################################################################################################
+
+
 
 bot.run(TOKEN)
